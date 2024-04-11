9a640d026c93733c4e1ac4598165d7d4994cbe36
mohamedanter1996 <mohamedanter461@yahoo.com>
2024-04-11 13:06:51 +0200

    Part 15 Get Product By Id Endpoint

--- Talabat.APIs/Controllers/ProductsController.cs
+++ Talabat.APIs/Controllers/ProductsController.cs
@@ -21,10 +21,25 @@ namespace Talabat.APIs.Controllers
 
 		public async Task<ActionResult<IEnumerable<Product>>> GetProducts()
 		{
-			var products=await _productRepo.GetAllAsync();
+			var products = await _productRepo.GetAllAsync();
 			return Ok(products);
 		}
 
+		// api/Products/1
+
+		[HttpGet("{id}")]
+
+		public async Task<ActionResult<Product>> GetProduct(int id)
+		{
+			var product= await _productRepo.GetAsync(id);
+
+			if (product is null)
+			{
+				return NotFound(new {Message="Not Found",StatusCode=404}); //404
+			}
+
+			return Ok(product); //200
+		}
 	}
 
 }
