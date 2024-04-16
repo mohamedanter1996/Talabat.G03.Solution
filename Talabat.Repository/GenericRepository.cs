52427a24694ac9eb1dad4d4c8b7f6755e0233794
mohamedanter1996 <mohamedanter461@yahoo.com>
2024-04-16 02:40:32 +0200

    Part 01 Specification Design Pattern

--- Talabat.Repository/GenericRepository.cs
+++ Talabat.Repository/GenericRepository.cs
@@ -6,6 +6,8 @@ using System.Text;
 using System.Threading.Tasks;
 using Talabat.Core.Entities;
 using Talabat.Core.Repositories.Contract;
+using Talabat.Core.Specifications;
+using Talabat.infrastructure;
 using Talabat.Repository.Data;
 
 namespace Talabat.Repository
@@ -20,21 +22,40 @@ namespace Talabat.Repository
 		}
         public async Task<IEnumerable<T>> GetAllAsync()
 		{
-			if (typeof(T) == typeof(Product))
-			{
-				return (IEnumerable<T>) await _dbContext.Set<Product>().Include(P => P.Brand).Include(P => P.Category).ToListAsync();
-			}
+			//if (typeof(T) == typeof(Product))
+			//{
+			//	return (IEnumerable<T>) await _dbContext.Set<Product>().Include(P => P.Brand).Include(P => P.Category).ToListAsync();
+			//}
 			return await _dbContext.Set<T>().ToListAsync();
 		}
 
 		public async Task<T?> GetAsync(int id)
 		{
 
-			if (typeof(T) == typeof(Product))
-			{
-				return await _dbContext.Set<Product>().Where(P=>P.Id==id).Include(P => P.Brand).Include(P => P.Category).FirstOrDefaultAsync() as T;
-			}
+			//if (typeof(T) == typeof(Product))
+			//{
+			//	return await _dbContext.Set<Product>().Where(P=>P.Id==id).Include(P => P.Brand).Include(P => P.Category).FirstOrDefaultAsync() as T;
+			//}
 			return await _dbContext.Set<T>().FindAsync(id);
+		}
+
+	
+
+		async Task<IEnumerable<T>> IGenericRepository<T>.GetAllWithSpecAsync(Ispecifications<T> spec)
+		{
+			return await ApplySpecifications(spec).ToListAsync();
+		}
+
+
+
+		async Task<T?> IGenericRepository<T>.GetWithSpecAsync(Ispecifications<T> spec)
+		{
+			return await ApplySpecifications(spec).FirstOrDefaultAsync();
+		}
+
+		private IQueryable<T> ApplySpecifications(Ispecifications<T> spec)
+		{
+			return SpecificationsEvaluator<T>.GetQuery(_dbContext.Set<T>(), spec);
 		}
 	}
 }
