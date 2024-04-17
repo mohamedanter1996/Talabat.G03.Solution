580c7ebf7a55970252f741b7c7447f87dff1c59c
mohamed anter <mohamedanter954@gmail.com>
2024-04-17 11:51:36 +0200

    Part 12 3-Ways to Create Middleware

--- Talabat.APIs/Middlewares/ExceptionsMiddleware.cs
+++ Talabat.APIs/Middlewares/ExceptionsMiddleware.cs
@@ -1,10 +1,11 @@
-﻿using System.Net;
+﻿using Microsoft.AspNetCore.Http;
+using System.Net;
 using System.Text.Json;
 using Talabat.APIs.Errors;
 
 namespace Talabat.APIs.Middlewares
 {
-	public class ExceptionsMiddleware
+	public class ExceptionsMiddleware //: IMiddleware
 	{
 		private readonly RequestDelegate _next;
 		private readonly ILogger<ExceptionsMiddleware> _logger;
@@ -16,28 +17,28 @@ namespace Talabat.APIs.Middlewares
 			_logger = loggerFactory;
 			_env = env;
 		}
-		public async Task InvokeAsync(HttpContext httpcontext)
-		{
-			try
-			{
-				await _next.Invoke(httpcontext);
-			}
-			catch (Exception ex)
-			{
-
-				_logger.LogError(ex.Message);
-				httpcontext.Response.StatusCode = (int)HttpStatusCode.InternalServerError;
-				httpcontext.Response.ContentType = "application/json";
-
-				var response = _env.IsDevelopment() ?
-					new ApiExceptionsResponse((int)HttpStatusCode.InternalServerError, ex.Message, ex.StackTrace.ToString())
-					: new ApiExceptionsResponse((int)HttpStatusCode.InternalServerError);
-				var options = new JsonSerializerOptions() { PropertyNamingPolicy = JsonNamingPolicy.CamelCase };
-				var json = JsonSerializer.Serialize(response, options);
-
-				httpcontext.Response.WriteAsync(json);
-			}
-
-		}
+		//public async Task InvokeAsync(HttpContext httpcontext)
+		//{
+		//	try
+		//	{
+		//		await _next.Invoke(httpcontext);
+		//	}
+		//	catch (Exception ex)
+		//	{
+		//
+		//		_logger.LogError(ex.Message);
+		//		httpcontext.Response.StatusCode = (int)HttpStatusCode.InternalServerError;
+		//		httpcontext.Response.ContentType = "application/json";
+		//
+		//		var response = _env.IsDevelopment() ?
+		//			new ApiExceptionsResponse((int)HttpStatusCode.InternalServerError, ex.Message, ex.StackTrace.ToString())
+		//			: new ApiExceptionsResponse((int)HttpStatusCode.InternalServerError);
+		//		var options = new JsonSerializerOptions() { PropertyNamingPolicy = JsonNamingPolicy.CamelCase };
+		//		var json = JsonSerializer.Serialize(response, options);
+		//
+		//		httpcontext.Response.WriteAsync(json);
+		//	}
+		//
+		//}
 	}
 }
