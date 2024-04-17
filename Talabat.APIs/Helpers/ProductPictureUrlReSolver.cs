105a6e609484037ff79f736a58274921b6fce1c6
mohamed anter <mohamedanter954@gmail.com>
2024-04-17 09:39:52 +0200

    Part 06 Product Picture Url Resolver

---
+++ Talabat.APIs/Helpers/ProductPictureUrlReSolver.cs
@@ -0,0 +1,25 @@
+﻿using AutoMapper;
+using Microsoft.Extensions.Configuration;
+using Talabat.APIs.Dtos;
+using Talabat.Core.Entities;
+using static System.Net.WebRequestMethods;
+
+namespace Talabat.APIs.Helpers
+{
+	public class ProductPictureUrlReSolver : IValueResolver<Product, ProductToReturnDto, string>
+	{
+		private readonly IConfiguration _configuration;
+		public ProductPictureUrlReSolver(IConfiguration configuration)
+		{
+			_configuration = configuration;
+		}
+		public string Resolve(Product source, ProductToReturnDto destination, string destMember, ResolutionContext context)
+		{
+			if (!string.IsNullOrEmpty(source.PictureUrl))
+			{
+				return $"{_configuration["ApiBaseUrl"]}/{source.PictureUrl}";
+			}
+			return string.Empty;
+		}
+	}
+}
